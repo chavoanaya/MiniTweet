87393a17e512491975886f55ed37b741bfd5d8af
chavoanaya <ag.scalderon@gmail.com>
2023-11-18 19:42:20 +0100

    botones de like y de mensajes en las cajas del feed

--- src/Componentes/Feed.jsx
+++ src/Componentes/Feed.jsx
@@ -15,27 +15,27 @@ function Feed() {
         <div class="list-group mt-2">
   <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
     <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
+      <h5 class="mb-1">David Letterman</h5>
       <small>3 days ago</small>
     </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <small>And some small print.</small>
+    <p class="mb-1">I belive theres no better comedian than chavo anaya</p>
+    <small><i class="bi bi-chat"></i> <i class="bi bi-hand-thumbs-up"></i></small>
   </a>
   <a href="#" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
+      <h5 class="mb-1">Chavo.anaya</h5>
       <small class="text-body-secondary">3 days ago</small>
     </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <small class="text-body-secondary">And some muted small print.</small>
+    <p class="mb-1">Siento que las personas hablan mal de mi</p>
+    <small> <i class="bi bi-chat"></i> <i class="bi bi-hand-thumbs-up"></i></small>
   </a>
   <a href="#" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
+      <h5 class="mb-1">Arantxa1992</h5>
       <small class="text-body-secondary">3 days ago</small>
     </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <small class="text-body-secondary">And some muted small print.</small>
+    <p class="mb-1">Tengo suerte de estar casada con alguien tan guapo</p>
+    <small><i class="bi bi-chat"></i> <i class="bi bi-hand-thumbs-up"></i></small>
   </a>
 </div>
       </div>
